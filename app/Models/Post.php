a4dab086f98bd31314acb96d0f47d5d354a96658
enespolat24 <enespolat24@gmail.com>
2023-11-09 00:16:57 +0300

    slug bugfix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,7 +26,7 @@ class Post extends Model
     public function getSlugOptions(): SlugOptions
     {
         return SlugOptions::create()
-            ->generateSlugsFrom('name')
+            ->generateSlugsFrom('title')
             ->saveSlugsTo('slug');
     }
 
