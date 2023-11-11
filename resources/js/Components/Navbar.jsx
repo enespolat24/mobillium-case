6a8e6c34a598669cc72199a28b57aa4be0119ba0
enespolat24 <enespolat24@gmail.com>
2023-11-11 14:18:33 +0300

    auth dropdown

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "@inertiajs/react";
+import Dropdown from "./Dropdown";
+
 function Navbar({ auth }) {
-    console.log(auth);
     return (
         <nav className="bg-gray-800 p-4">
             <div className="container mx-auto">
@@ -12,26 +13,46 @@ function Navbar({ auth }) {
                         mobillium blog
                     </Link>
 
-                    {auth.user &
-                    (
-                        <>
-                            <Link
-                                href={route("login")}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Log in
-                            </Link>
+                    <div className="flex items-center space-x-4">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <span className="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                    >
+                                        {auth.user?.name}
 
-                            <Link
-                                href={route("register")}
-                                className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Register
-                            </Link>
-                        </>
-                    )}
+                                        <svg
+                                            className="ms-2 -me-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </Dropdown.Trigger>
 
-                    <div className="space-x-4">
+                            <Dropdown.Content>
+                                <Dropdown.Link href={route("profile.edit")}>
+                                    Profile
+                                </Dropdown.Link>
+                                <Dropdown.Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                >
+                                    Log Out
+                                </Dropdown.Link>
+                            </Dropdown.Content>
+                        </Dropdown>
+
                         {auth.user && auth.user.has_admin_role && (
                             <Link
                                 href={route("admin.dashboard")}
@@ -40,6 +61,7 @@ function Navbar({ auth }) {
                                 Admin Panel
                             </Link>
                         )}
+
                         {auth.user && auth.user.has_author_role && (
                             <Link
                                 href={route("authors.index")}
